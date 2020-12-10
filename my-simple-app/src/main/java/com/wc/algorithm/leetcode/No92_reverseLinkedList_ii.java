d4957d109f020cf51692a5a06fcfce8a26958d6f
shovel <wongtry@163.com>
2020-12-10 12:21:01 +0800

    反转部分单链表

--- my-simple-app/src/main/java/com/wc/algorithm/leetcode/No92_reverseLinkedList_ii.java
+++ my-simple-app/src/main/java/com/wc/algorithm/leetcode/No92_reverseLinkedList_ii.java
@@ -25,7 +25,53 @@ public class No92_reverseLinkedList_ii {
             return head;
         }
 
-        // 认为head之后的序列已经反转完成，即1->   <-2<-3<-4<-5
-        return null;
+        // 以下两指针用户反转
+        ListNode pre = null;
+        ListNode cur = head;
+        while (m > 1) {
+            pre = cur;
+            cur = cur.next;
+            m--;
+            n--;
+        }
+        ListNode firstPart = pre;
+        ListNode secondPart = cur;
+
+        // 反转
+        while (n > 0) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+            n--;
+        }
+
+        // 不是从最开始反转，将第一部分与反转后的链头连接
+        if (firstPart != null) {
+            firstPart.next = pre;
+        }
+        // 从最开始反转的，直接将反转后的链头作为head
+        else {
+            head = pre;
+        }
+        secondPart.next = cur;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println(ListNode.toString(node1));
+
+        No92_reverseLinkedList_ii no92 = new No92_reverseLinkedList_ii();
+        ListNode reverseList = no92.reverseBetween(node1, 2, 5);
+        System.out.println(ListNode.toString(reverseList));
     }
 }
